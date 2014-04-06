f0ef5304b4f9a79b1b381364c822ad52ec9019d2
britzl <bjorn.ritzl@gmail.com>
2014-04-06 16:02:08 +0200

    Initial commit of prettyprint.lua

---
+++ src/prettyprint.lua
@@ -0,0 +1,43 @@
+-- keep the original print()
+local _print = print
+
+local indentation = ""
+
+--- Pretty print a value
+-- If the value is a table it's content will be printed as well (recursively)
+local function pretty_print(value)
+	local t = type(value)
+	if t == "nil" then
+		_print(indentation .. "nil")
+	elseif t ~= "table" then
+		_print(indentation .. value)
+	else
+		_print(indentation .. "{")
+		indentation = indentation .. "\t"
+		for name,data in pairs(value) do
+			if type(data) == "table" then
+				_print(indentation .. name .. " =")
+				pretty_print(data)
+			else
+				_print(indentation .. name .. " = " .. tostring(data))
+			end
+		end
+		indentation = indentation:sub(1,#indentation-1)
+		_print(indentation .. "}")
+	end
+end
+
+--- Improved print() function
+-- If multiple arguments are passed they will be printed using the normal
+-- print() function.
+-- If a single argument is passed it will be pretty printed if it's a table
+local function improved_print(...)
+	if select('#',...) > 1 then
+		_print(...)
+	else
+		pretty_print(select(1, ...))
+	end
+end
+
+-- replace print() with improved print function that will pretty print tables
+print = improved_print
