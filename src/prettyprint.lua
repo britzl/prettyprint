8e6bf46734d33f461400a873f73bbb36a5f29187
britzl <bjorn.ritzl@gmail.com>
2014-09-28 10:36:36 +0200

    Prevent infinite loops

--- src/prettyprint.lua
+++ src/prettyprint.lua
@@ -1,56 +1,83 @@
 ----------------------------------------
--- print() replacement
--- Using print() with a single argument will pretty print tables
--- Using print() with multiple arguments will use the standard print() function
--- 
+-- print() replacement that will pretty print tables
+--
+-- @module prettyprint
 -- @author Björn Ritzl
 -- @license Apache License 2.0
 -- @github https://github.com/britzl/prettyprint
 ----------------------------------------
 
--- keep the original print()
-local _print = print
 
-local indentation = ""
+local pp = {
+	print = print,
+	indentation = "\t"
+}
 
---- 
--- Pretty print a table and it's contents (recursively)
--- @param #table value The table to print
-local function print_table(value)
-	_print(indentation .. "{")
-	indentation = indentation .. "\t"
-	for name,data in pairs(value) do
-		if type(name) == "string" then
-			name = '"'..name..'"'
-		else
-			name = tostring(name)
-		end
-		local dt = type(data)
-		if dt == "table" then
-			_print(indentation .. name .. " =")
-			print_table(data)
-		elseif dt == "string" then
-			_print(indentation .. name .. ' = "' .. tostring(data) .. '"')
-		else
-			_print(indentation .. name .. " = " .. tostring(data))
-		end
-	end
-	indentation = indentation:sub(1,#indentation-1)
-	_print(indentation .. "}")
-end
 
---- 
--- Improved print() function
--- If a single argument is passed and it's a table it will be pretty printed,
--- otherwise the argument(s) will be passed to the normal print() function
 local function improved_print(...)
-	local arg_count = select('#',...)
-	if arg_count == 1 and type(select(1, ...)) == "table" then
-		print_table(select(1, ...))
-	else
-		_print(...)
+	-- keep track of printed tables to avoid infinite loops
+	-- when tables refer to each other
+	local printed_tables = {}
+
+	local current_indentation = ""
+
+	local function indent_more()
+		current_indentation = current_indentation .. pp.indentation
 	end
+
+	local function indent_less()
+		current_indentation = current_indentation:sub(1, #current_indentation - #pp.indentation)
+	end
+
+	local function prindented(s)
+		pp.print(current_indentation .. s)
+	end
+
+	local function print_table(value)
+		prindented("{")
+		indent_more()
+		for name,data in pairs(value) do
+			if type(name) == "string" then
+				name = '"'..name..'"'
+			else
+				name = tostring(name)
+			end
+			local dt = type(data)
+			if dt == "table" then
+				prindented(name .. " = [".. tostring(data) .. "]")
+				if not printed_tables[data] then
+					printed_tables[data] = true
+					print_table(data)
+				end
+			elseif dt == "string" then
+				prindented(name .. ' = "' .. tostring(data) .. '"')
+			else
+				prindented(name .. " = " .. tostring(data))
+			end
+		end
+		indent_less()
+		prindented("}")
+	end
+
+	-- iterate through each of the arguments and print them one by one
+	local args = { ... }
+	table.remove(args, 1)
+	local s = ""
+	for _, v in pairs(args) do
+		if type(v) == "table" then
+			pp.print(s)
+			s = ""
+			print_table(v)
+		else
+			s = s .. tostring(v) .. "\t"
+		end
+	end
+	pp.print(s)
 end
 
--- replace print() with improved print function that will pretty print tables
-print = improved_print
+print = setmetatable(pp, {
+	__call = improved_print,
+	__AUTHOR = "Björn Ritzl",
+	__DESCRIPTION = "print() replacement that will pretty-print tables",
+	__URL = "https://github.com/britzl/prettyprint",
+})
